45cb68482fbd21fb044c892b4b0410c72e30a754
Oscar Costa <oscarcosta@gmail.com>
2023-09-03 16:45:56 -0700

    Adding comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # photoMetaData
 Python script to extract photo exif
+
+- main.py: reads exif of image files from a directory and save data to a csv file
+- extract_exif.py: testing a few libs to extract exif
+- 
--- extract_exif.py
+++ extract_exif.py
@@ -5,7 +5,7 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 print('PIL output')
-image_path = sys.argv[1]
+image_path = sys.argv[1]  # input file name
 image = Image.open(image_path)
 exif_data = image.getexif()
 for tag, value in exif_data.items():
@@ -16,7 +16,6 @@ print('pyexiv2 output')
 with open(image_path, 'rb') as f:
     with pyexiv2.ImageData(f.read()) as img:
         exif_data = img.read_exif()
-
         for tag, value in exif_data.items():
             tag_name = TAGS.get(tag, tag)
             print(f'{tag_name}: {value}')
